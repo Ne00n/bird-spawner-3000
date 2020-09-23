28cbafb4d14b0b954f2902fdef9e29b5a7f74136
Neo <neo@tiefkuehler-madness.me>
2020-09-23 14:21:52 +0200

    add latency

---
+++ latency.py
@@ -0,0 +1,60 @@
+import subprocess, json, re
+
+class Latency:
+    def cmd(self,cmd):
+        p = subprocess.run(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        return [p.stdout.decode('utf-8'),p.stderr.decode('utf-8')]
+
+    def parse(self,configRaw):
+        parsed = re.findall('interface "(pipe.*?)".*?([0-9.]+).*?cost ([0-9]+)',configRaw, re.DOTALL)
+        data = []
+        for nic,target,weight in parsed:
+            data.append({'nic':nic,'target':target,'weight':weight})
+        return data
+
+    def getAvrg(self,row):
+        n,result = 12,0
+        for index,entry in enumerate(row):
+            if index <= n:
+                result += float(entry[0])
+        return int(float(result / 13) * 100)
+
+    def getLatency(self,config):
+        fping = ["fping", "-c", "15"]
+        for row in config:
+            fping.append(row['target'])
+        result = subprocess.run(fping, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        parsed = re.findall("([0-9.]+).*?([0-9]+.[0-9]).*?([0-9])% loss",result.stdout.decode('utf-8'), re.MULTILINE)
+        latency =  {}
+        for ip,ms,loss in parsed:
+            if ip not in latency:
+                latency[ip] = []
+            latency[ip].append([ms,loss])
+        for entry,row in latency.items():
+            row.sort()
+        for node in list(config):
+            for entry,row in latency.items():
+                if entry == node['target']:
+                    node['latency'] = self.getAvrg(row)
+                elif node['target'] not in latency and nic in config:
+                    print("Warning: cannot reach",node['target'],"skipping")
+                    del config[node['nic']]
+        if (len(config) != len(latency)):
+            print("Warning: Targets do not match expected responses.")
+        return config
+
+L = Latency()
+try:
+    #Check if bird is running
+    L.cmd("pgrep bird")
+    #Getting config
+    configRaw = L.cmd("cat /etc/bird/bird.conf")[0]
+    #Parsing
+    config = L.parse(configRaw)
+    #fping
+    result = L.getLatency(config)
+    print(result)
+    #filter out anything with less or equal than 100 = 1ms change
+
+except Exception as e:
+    print("Error:",str(e))
