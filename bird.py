0e1f9e4b6a47a88c796c989b8d6b13a6afa3434d
Neo <neo@tiefkuehler-madness.me>
2020-12-11 01:45:10 +0100

    pass config param

--- bird.py
+++ bird.py
@@ -1,11 +1,13 @@
 from Class.bird import Bird
 import sys
-param = sys.argv[1]
 print("Bird Spawner 3000")
-bird = Bird()
-if param == "build":
-    bird.run()
-elif param == "shutdown":
-    bird.shutdown()
-else:
+config = "hosts.json"
+if len(sys.argv) > 2:
+    config = sys.argv[2]
+bird = Bird(config)
+if len(sys.argv) == 1:
     print("build, shutdown")
+elif sys.argv[1] == "build":
+    bird.run(config)
+elif sys.argv[1] == "shutdown":
+    bird.shutdown()
