a2a1896f4a2a460e5c16c7649d293a8b2d3adbab
Neoon <neo@tiefkuehler-madness.me>
2021-11-21 09:44:57 +0100

    depending on traffic, switch between 60s and 120s checks

--- latency.py
+++ latency.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import subprocess, datetime, json, time, sys, re
+from ipaddress import ip_network
 from datetime import datetime
 from random import randint
 from pathlib import Path
 
 class Latency:
     def __init__(self):
-        self.files = {"peering.json":{},'longtime.json':{}}
+        self.files = {"peering.json":{},'longtime.json':{},'traffic.json':{}}
+        cron = [2,18,32,48]
         self.long,self.file = False,"peering.json"
-        if len(sys.argv) == 2 and sys.argv[1] == "longtime" or datetime.now().minute == 0:
+        if len(sys.argv) == 2 and sys.argv[1] == "longtime" or datetime.now().minute in cron:
             self.file = "longtime.json"
             self.long = True
-        files = ["peering.json","longtime.json"]
+        files = ["peering.json","longtime.json",'traffic.json']
         for file in files:
             if Path(file).exists():
                 print("Loading",file)
@@ -27,10 +29,18 @@ class Latency:
         print(f"Saving {self.file}")
         with open(self.file, 'w') as f:
             json.dump(self.files[self.file], f, indent=4)
+        print("Saving traffic.json")
+        with open("traffic.json", 'w') as f:
+            json.dump(self.files["traffic.json"], f, indent=4)
 
     def cmd(self,cmd):
         p = subprocess.run(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         return [p.stdout.decode('utf-8'),p.stderr.decode('utf-8')]
+
+    def sameNetwork(self,origin,target):
+        o = ip_network(origin, strict = False).network_address
+        t = ip_network(target, strict = False).network_address
+        return o == t
 
     def parse(self,configRaw):
         parsed = re.findall('interface "([a-zA-Z0-9]{3,}?)".*?([0-9.]+).*?cost ([0-9]+)',configRaw, re.DOTALL)
@@ -38,6 +48,30 @@ class Latency:
         for nic,target,weight in parsed:
             data.append({'nic':nic,'target':target,'weight':weight})
         return data
+
+    def filter(self,config):
+        traffic = L.cmd('ifconfig')
+        data = re.findall("inet\s([0-9.]+).*?bytes\s([0-9]+)",traffic[0], re.MULTILINE | re.DOTALL)
+        diff = {}
+        for row in data:
+            if self.files["traffic.json"]:
+                for ip,traffic in self.files["traffic.json"].items():
+                    if row[0] == ip:
+                        diff[ip] = int(row[1]) - int(traffic)
+                        break
+            self.files['traffic.json'][row[0]] = row[1]
+
+        if datetime.now().minute % 2 != 0:
+            print("Filtering Config")
+            for ip,bytes in diff.items():
+                if bytes < 500000:
+                    for row in list(config):
+                        if self.sameNetwork(f"{ip}/31",f"{row['target']}/31"):
+                            print(f"Dropping {row['target']} due to low traffic")
+                            del row
+        else:
+            print("Skipping Filtering Config")
+        return config
 
     def getAvrg(self,row,weight=False):
         result = 0
@@ -111,18 +145,20 @@ for run in range(3):
     fping = L.cmd("pgrep fping")
     if fping[0] == "": break
     if run == 2: raise Exception('fping is running, exiting.')
-    time.sleep(randint(10, 20))
+    time.sleep(randint(5, 10))
 #Getting config
 print("Reading bird config")
 configRaw = L.cmd("cat /etc/bird/bird.conf")[0].rstrip()
 #Parsing
 config = L.parse(configRaw)
 print("Waiting for deplayed fping")
-time.sleep(randint(2,30))
+time.sleep(randint(2,20))
+#Filter Config
+config = L.filter(config)
 #fping
 print("Running fping")
 if L.isLongtime():
-    result = L.getLatency(config,300)
+    result = L.getLatency(config,60)
 else:
     result = L.getLatency(config)
 #update
@@ -137,7 +173,7 @@ if not result:
 else:
     #push
     print("Waiting for deplayed update")
-    time.sleep(randint(5,40))
+    time.sleep(randint(2,20))
     print("Writing config")
     L.cmd("echo '"+configRaw+"' > /etc/bird/bird.conf")
     #reload
