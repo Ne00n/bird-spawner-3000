4fc68150f00d60d5688be667613357ed527a34f2
Neoon <neo@tiefkuehler-madness.me>
2022-07-16 10:37:18 +0200

    remove long, just 30s interval

--- latency.py
+++ latency.py
@@ -7,11 +7,8 @@ from pathlib import Path
 
 class Latency:
     def __init__(self):
-        self.configFiles,cron = ['peering.json','longtime.json'],[5,15,25,35,45,55]
-        self.long,self.file,self.files = False,"peering.json",{}
-        if len(sys.argv) == 2 and sys.argv[1] == "longtime" or datetime.now().minute in cron:
-            self.file = "longtime.json"
-            self.long = True
+        self.configFiles = ['peering.json']
+        self.file,self.files = "peering.json",{}
         for file in self.configFiles:
             if Path(file).exists():
                 print("Loading",file)
@@ -22,9 +19,6 @@ class Latency:
                     self.files[file] = {"created":int(datetime.now().timestamp())}
             else:
                 self.files[file] = {"created":int(datetime.now().timestamp())}
-
-    def isLongtime(self):
-        return self.long
 
     def save(self):
         for file in self.configFiles:
@@ -61,8 +55,9 @@ class Latency:
             if float(entry[0]) > avrg + grace: return True
         return False
 
-    def getLatency(self,config,pings=6):
+    def getLatency(self,config,pings=4,isClient=False):
         fping = ["fping", "-c", str(pings)]
+        clients = ["PI","CLIENT"]
         for row in config:
             fping.append(row['target'])
         result = subprocess.run(fping, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -80,40 +75,38 @@ class Latency:
         total,loss,jittar = 0,0,0
         for node in list(config):
             for entry,row in latency.items():
+                isClientLink = True  if any(cl in node['nic'] for cl in clients) else False
                 if entry == node['target']:
                     node['latency'] = self.getAvrg(row)
-                    if entry not in tempFile['longtime.json']: tempFile['longtime.json'][entry] = {"packetloss":0,"jitter":0}
                     if entry not in tempFile['peering.json']: tempFile['peering.json'][entry] = {"packetloss":0,"jitter":0}
 
-                    hadLossLong = tempFile['longtime.json'][entry]['packetloss'] > current
                     hadLoss = tempFile['peering.json'][entry]['packetloss'] > current
                     hasLoss = len(row) < pings -1
 
-                    if hasLoss or hadLoss or hadLossLong:
+                    if hasLoss or hadLoss:
                         node['latency'] = node['latency'] + 5000 #+ 50ms / weight
                         loss = loss +1
 
                     if hasLoss:
                         tempFile[self.file][entry]['packetloss'] = current + 1800 #update event
                         print(entry,"Packetloss detected","got",len(row),f"of {pings -1}, adding penalty")
-                    elif hadLoss or hadLossLong:
+                    elif hadLoss:
                         print(entry,"Ongoing Packetloss")
 
-                    hadJitterLong = tempFile['longtime.json'][entry]['jitter'] > current
                     hasJitter = self.hasJitter(row,self.getAvrg(row,True))
                     hadJitter = tempFile['peering.json'][entry]['jitter'] > current
 
-                    if hasJitter or hadJitter or hadJitterLong:
-                        node['latency'] = node['latency'] + 1000 #+ 10ms /weight
-                        jittar = jittar +1
+                    if isClient == False and isClientLink == False:
+                        if hasJitter or hadJitter:
+                            node['latency'] = node['latency'] + 1000 #+ 10ms /weight
+                            jittar = jittar +1
 
-                    if hasJitter:
-                        extend = 3600 if self.isLongtime() else 900
-                        tempFile[self.file][entry]['jitter'] = current + extend #update event
-                        print(entry,"High Jitter dectected, adding penalty")
-                    elif hadJitter or hadJitterLong:
-                        print(entry,"Ongoing Jitter")
-                    total = total +1
+                        if hasJitter:
+                            tempFile[self.file][entry]['jitter'] = current + 900 #update event
+                            print(entry,"High Jitter dectected, adding penalty")
+                        elif hadJitter:
+                            print(entry,"Ongoing Jitter")
+                        total = total +1
 
         print (f"Total {total}, Jitter {jittar}, Packetloss {loss}")
         return config
@@ -129,33 +122,32 @@ for run in range(3):
     if fping[0] == "": break
     if run == 2: raise Exception('fping is running, exiting.')
     time.sleep(randint(5, 10))
-#Getting config
-print("Reading bird config")
-configRaw = L.cmd("cat /etc/bird/bird.conf")[0].rstrip()
-#Parsing
-config = L.parse(configRaw)
-#fping
-print("Running fping")
-if L.isLongtime():
-    result = L.getLatency(config,60)
-else:
-    result = L.getLatency(config)
-#update
-configs = L.cmd('ip addr show')
-local = re.findall("inet (10\.0[0-9.]+\.1)\/(32|30) scope global lo",configs[0], re.MULTILINE | re.DOTALL)
-configRaw = re.sub(local[0][0]+"; #updated [0-9]+", local[0][0]+"; #updated "+str(int(time.time())), configRaw, 0, re.MULTILINE)
-for entry in result:
-    if "latency" not in entry: entry['latency'] = 65000
-    configRaw = re.sub("cost "+str(entry['weight'])+"; #"+entry['target'], "cost "+str(entry['latency'])+"; #"+entry['target'], configRaw, 0, re.MULTILINE)
-if not result:
-    print("Nothing to do")
-else:
-    #push
-    print("Waiting for deplayed update")
-    time.sleep(randint(2,20))
-    print("Writing config")
-    L.cmd("echo '"+configRaw+"' > /etc/bird/bird.conf")
-    #reload
-    print("Reloading bird")
-    L.cmd('/usr/sbin/service bird reload')
-L.save()
+for run in range(2):
+    #Getting config
+    print("Reading bird config")
+    configRaw = L.cmd("cat /etc/bird/bird.conf")[0].rstrip()
+    #Parsing
+    config = L.parse(configRaw)
+    configs = L.cmd('ip addr show')
+    isClient = "10.0.250" in configs[0]
+    #fping
+    print("Running fping")
+    time.sleep(randint(0,5))
+    result = L.getLatency(config,6,isClient)
+    #update
+    local = re.findall("inet (10\.0[0-9.]+\.1)\/(32|30) scope global lo",configs[0], re.MULTILINE | re.DOTALL)
+    configRaw = re.sub(local[0][0]+"; #updated [0-9]+", local[0][0]+"; #updated "+str(int(time.time())), configRaw, 0, re.MULTILINE)
+    for entry in result:
+        if "latency" not in entry: entry['latency'] = 65000
+        configRaw = re.sub("cost "+str(entry['weight'])+"; #"+entry['target'], "cost "+str(entry['latency'])+"; #"+entry['target'], configRaw, 0, re.MULTILINE)
+    if not result:
+        print("Nothing to do")
+    else:
+        #write
+        print("Writing config")
+        L.cmd("echo '"+configRaw+"' > /etc/bird/bird.conf")
+        #reload
+        print("Reloading bird")
+        L.cmd('/usr/sbin/service bird reload')
+    L.save()
+    if run == 0: time.sleep(30)
